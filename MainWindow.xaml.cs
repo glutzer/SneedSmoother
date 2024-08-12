1a2e2c3b6df5bc489c31e56d4f4b2f987464c4c7
glutzer <glutzer@glutzer.com>
2024-08-12 16:55:32 -0500

    test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,9 +5,6 @@ using System.Windows;
 
 namespace PoeFixer;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public string? GGPKPath { get; set; }
